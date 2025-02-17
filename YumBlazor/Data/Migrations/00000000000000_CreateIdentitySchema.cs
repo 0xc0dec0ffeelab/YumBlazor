e15a4c6d9fe201ccc33206b3d02466548a65e877
tingwei628 <sbvq6p199xe@gmail.com>
2025-02-18 05:24:57 +0800

    fix: migrations

--- YumBlazor/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ YumBlazor/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -15,11 +15,11 @@ namespace YumBlazor.Migrations
 				name: "AspNetRoles",
 				columns: table => new
 				{
-					Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-					NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-					ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
-				},
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "text", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetRoles", x => x.Id);
@@ -29,22 +29,22 @@ namespace YumBlazor.Migrations
 				name: "AspNetUsers",
 				columns: table => new
 				{
-					Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-					NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-					Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-					NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-					EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
-					PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
-					TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
-					LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
-					LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
-					AccessFailedCount = table.Column<int>(type: "int", nullable: false)
-				},
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    UserName = table.Column<string>(type: "text", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "text", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "text", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "text", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetUsers", x => x.Id);
@@ -54,12 +54,11 @@ namespace YumBlazor.Migrations
 				name: "AspNetRoleClaims",
 				columns: table => new
 				{
-					Id = table.Column<int>(type: "int", nullable: false)
-						.Annotation("SqlServer:Identity", "1, 1"),
-					RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
-				},
+                    Id = table.Column<int>(type: "serial", nullable: false),
+                    RoleId = table.Column<string>(type: "text", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
@@ -75,12 +74,11 @@ namespace YumBlazor.Migrations
 				name: "AspNetUserClaims",
 				columns: table => new
 				{
-					Id = table.Column<int>(type: "int", nullable: false)
-						.Annotation("SqlServer:Identity", "1, 1"),
-					UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
-				},
+                    Id = table.Column<int>(type: "serial", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
@@ -96,11 +94,11 @@ namespace YumBlazor.Migrations
 				name: "AspNetUserLogins",
 				columns: table => new
 				{
-					LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					ProviderKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-					UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
-				},
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    ProviderKey = table.Column<string>(type: "text", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
@@ -116,9 +114,9 @@ namespace YumBlazor.Migrations
 				name: "AspNetUserRoles",
 				columns: table => new
 				{
-					UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
-				},
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<string>(type: "text", nullable: false)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
@@ -140,11 +138,11 @@ namespace YumBlazor.Migrations
 				name: "AspNetUserTokens",
 				columns: table => new
 				{
-					UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
-					Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
-				},
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
 				constraints: table =>
 				{
 					table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
@@ -156,45 +154,43 @@ namespace YumBlazor.Migrations
 						onDelete: ReferentialAction.Cascade);
 				});
 
-			migrationBuilder.CreateIndex(
-				name: "IX_AspNetRoleClaims_RoleId",
-				table: "AspNetRoleClaims",
-				column: "RoleId");
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
 
-			migrationBuilder.CreateIndex(
-				name: "RoleNameIndex",
-				table: "AspNetRoles",
-				column: "NormalizedName",
-				unique: true,
-				filter: "[NormalizedName] IS NOT NULL");
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
 
-			migrationBuilder.CreateIndex(
-				name: "IX_AspNetUserClaims_UserId",
-				table: "AspNetUserClaims",
-				column: "UserId");
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
 
-			migrationBuilder.CreateIndex(
-				name: "IX_AspNetUserLogins_UserId",
-				table: "AspNetUserLogins",
-				column: "UserId");
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
 
-			migrationBuilder.CreateIndex(
-				name: "IX_AspNetUserRoles_RoleId",
-				table: "AspNetUserRoles",
-				column: "RoleId");
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
 
-			migrationBuilder.CreateIndex(
-				name: "EmailIndex",
-				table: "AspNetUsers",
-				column: "NormalizedEmail");
+            migrationBuilder.CreateIndex(
+                 name: "EmailIndex",
+                 table: "AspNetUsers",
+                 column: "NormalizedEmail");
 
-			migrationBuilder.CreateIndex(
-				name: "UserNameIndex",
-				table: "AspNetUsers",
-				column: "NormalizedUserName",
-				unique: true,
-				filter: "[NormalizedUserName] IS NOT NULL");
-		}
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+        }
 
 		/// <inheritdoc />
 		protected override void Down(MigrationBuilder migrationBuilder)
